4bf531c196fffb258c41164415149a558d008a3c
Tetsu ThugStyle4ever <thugstyleforever@gmail.com>
2013-11-28 16:36:51 +0900

    add check.php

--- check.php
+++ check.php
@@ -25,14 +25,14 @@
 				<p>次のフォームに必要事項を記入してください。</p>
 				<form action="" method="post" enctype="mulutipart/form-data">
 				  <dl>
-					  <dt>ニックネーム<span class="required">必須</span></dt>
-					  <dd><input type="text" name="name" size="35" maxlength="255" /></dd>
-					  <dt>メールアドレス<span class="required">必須</span></dt>
-					  <dd><input type="text" name="email" size="35" maxlength="255" /></dd>
-					  <dt>パスワード<span class="required">必須</span></dt>
-					  <dd><input type="password" name="password" size="10" maxlength="20" /></dd>
+					  <dt>ニックネーム</dt>
+					  <dd></dd>
+					  <dt>メールアドレス</dt>
+					  <dd></dd>
+					  <dt>パスワード</dt>
+					  <dd></dd>
 					  <dt>写真など</dt>
-					  <dd><input type="file" name="image" size="35" /></dd>
+					  <dd></dd>
 					</dl>
 				<div><input type="submit" value="入力内容を確認する" /></div>
 				</form>
