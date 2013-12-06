70790070aa66343c187fd0138fa8445ee406b43f
Tetsu ThugStyle4ever <thugstyleforever@gmail.com>
2013-12-06 10:46:41 +0900

    add delete.php view.php

--- index.php
+++ index.php
@@ -20,9 +20,10 @@
   //投稿を記録する
   if (!empty($_POST)) {
     if ($_POST['message'] != '') {
-      $sql = sprintf('INSERT INTO posts SET member_id=%d, message="%s", created=NOW()',
+      $sql = sprintf('INSERT INTO posts SET member_id=%d, message="%s", reply_post_id=%d, created=NOW()',
       mysql_real_escape_string($member['id']),
-      mysql_real_escape_string($_POST['message'])
+      mysql_real_escape_string($_POST['message']),
+      mysql_real_escape_string($_POST['reply_post_id'])
       );
       mysql_query($sql) or die(mysql_error());
 
@@ -32,10 +33,25 @@
   }
 
   //投稿を取得する
-  $sql = sprintf('SELECT m.name, m.picture, p. * FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC');
-  $posts = mysql_query($sql) or die(mysql_error());
+  // $sql = sprintf('SELECT m.name, m.picture, p. * FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC'); sprintfは意味が無い
+  $sql = 'SELECT m.name, m.picture, p. * FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC';
+  $posts = mysql_query($sql) or die(mysql_error()
+    );
 
-
+  //返信[Re]が押された場合
+  if (isset($_GET['res'])) {
+    $sql = sprintf('SELECT m.name, m.picture, p. * FROM members m, posts p WHERE m.id=p.member_id AND p.id=%d ORDER BY p.created DESC',
+      mysql_real_escape_string($_GET['res'])
+      );
+      $record = mysql_query($sql) or die(mysql_error());
+      $table = mysql_fetch_assoc($record);
+      $message = '@'. $table['name']. '&nbsp;&nbsp;' . $table['message'];
+      $res = htmlspecialchars($_GET['res'], ENT_QUOTES, 'UTF-8');
+  }
+  //本文内のURLリンクを設定
+  function makeLink($value){
+    return mb_ereg_replace("(https?)(://[[:alnum:]\+\$\;¥?\.%,!#~*/:@&=_-]+)", '<a href="\1\2">\1\2</a>', $value);
+  }
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -56,11 +72,12 @@
         <form action="" method="post">
           <dl>
             <dt>
-              <img src="member_picture/<?= $member['picture'] ?>" width="30" height="30" alt="" />
+              <img src="member_picture/<?= $member['picture'] ?>" width="35" height="35" alt="" />
               <?= htmlspecialchars($member['name']); ?>さん、入力してください
             </dt>
             <dd>
-              <textarea name="message" cols="50" rows="5"></textarea>
+              <textarea name="message" cols="50" rows="5"><?= $message ?></textarea>
+              <input type="hidden" name="reply_post_id" value="<?= $res ?>" />
             </dd>
           </dl>
           <div>
@@ -70,23 +87,45 @@
 
         <?php
         while ($post = mysql_fetch_assoc($posts)):
+
         $picture = htmlspecialchars($post['picture'], ENT_QUOTES, 'UTF-8');
         $name = htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8');
-        $message = str_replace("\n", "<br />", $post['message']);
+        $messa = str_replace("\n", "<br />", $post['message']);
         $created = htmlspecialchars($post['created'], ENT_QUOTES, 'UTF-8');
+        $id = htmlspecialchars($post['id'], ENT_QUOTES, 'UTF-8');
+        $rep = htmlspecialchars($post['reply_post_id'], ENT_QUOTES, 'UTF-8');
         ?>
 
         <div class="msg">
           <hr />
           <img src="member_picture/<?= $picture ?>" height="48" width="48" alt="<?= $name ?>" />
-          <p><?= $message ?><span class="name" id="tname">(<?= $name ?>)</span></p>
-          <p class="day"><?= $created ?></p>
-        </div>
+          <p><?= makeLink($messa) ?>
+            <span class="name" id="tname">(<?= $name ?>)</span>
+            [<a href="index.php?res=<?= $id ?>">Re</a>]
+          </p>
+          <p class="day" id="day"><a href="view.php?id=<?= $id ?><?= $created ?>"><?= $created ?></a>
 
+          <?php
+          if ($post['reply_post_id'] > 0):
+          ?>
+          <a href="view.php?id=<?= $rep ?>">返信元のメッセージ</a>
+          <?php
+          endif;
+          ?>
+
+          <?php
+            if ($_SESSION['id'] == $post['member_id']):
+          ?>
+            [<a href="delete.php?id=<?= $id ?>" style="color: #f33;">Delete</a>]
+          <?php
+          endif;
+          ?>
+
+          </p>
+        </div>
         <?php
         endwhile;
         ?>
-
       </div>
       <div id="foot">
         &copy;fakeTwitter&nbsp;2013&nbsp;all rights reserveed
---
+++ view.php
@@ -0,0 +1,66 @@
+<?php
+  session_start();
+  require('dbconnect.php');
+
+  if (empty($_GET['id'])) {
+      header('Location: index.php');
+      exit();
+    }
+
+  //投稿を取得する
+  $sql = sprintf('SELECT m.name, m.picture, p. * FROM members m, posts p WHERE m.id=p.member_id AND p.id=%d ORDER BY p.created DESC',
+  mysql_real_escape_string($_GET['id'])
+  );
+  $posts = mysql_query($sql) or die(mysql_error());
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <title>fakeTwitter</title>
+  </head>
+  <body id="fsz">
+    <div id="wrap">
+      <div id="head">
+        <h1>fakeTwitter</h1>
+      </div>
+
+      <div id="content">
+        <p>&laquo;<a href="index.php">一覧に戻る</a></p>
+
+        <?php
+        if ($post = mysql_fetch_assoc($posts)):
+
+        $picture = htmlspecialchars($post['picture'], ENT_QUOTES, 'UTF-8');
+        $name = htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8');
+        $messa = str_replace("\n", "<br />", $post['message']);
+        $created = htmlspecialchars($post['created'], ENT_QUOTES, 'UTF-8');
+        $id = htmlspecialchars($post['id'], ENT_QUOTES, 'UTF-8');
+        ?>
+
+        <div class="msg">
+          <hr />
+          <img src="member_picture/<?= $picture ?>" height="55" width="55" alt="<?= $name ?>" />
+          <p><?= $messa ?>
+            <span class="name" id="tname">(<?= $name ?>)</span>
+          </p>
+          <p class="day" id="day"><?= $created ?></p>
+        </div>
+
+        <?php
+        else:
+        ?>
+          <p>そのTweetは削除されたか、URLが間違っています</p>
+        <?php
+        endif;
+        ?>
+
+      </div>
+      <div id="foot">
+        &copy;fakeTwitter&nbsp;2013&nbsp;all rights reserveed
+      </div>
+    </div>
+  </body>
+</html>
